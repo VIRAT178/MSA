e62cc317c3c021c18c5ac9435a6bea252d625e5a
Vishal Pratap Singh <152519316+VIRAT178@users.noreply.github.com>
2026-06-30 13:29:53 +0530

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import cors from "cors";
 import fs from "fs";
 import crypto from "crypto";
 import { GoogleGenAI } from "@google/genai";
